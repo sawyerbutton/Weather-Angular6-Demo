d93978c30fd407eaeaef3b1d04af43f0011c6169
sawyerbutton <lil103@pitt.edu>
2018-05-28 16:20:57 -0400

    alpha version of weather demo on angular 6

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { DetailsComponent} from './details/details.component';
+import { AddCardComponent} from './add-card/add-card.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'details/:city', component: DetailsComponent},
+  {path: 'add', component: AddCardComponent},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'signup', component: SignupComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
